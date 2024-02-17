44e0da53fdac23b24c211109351e8a898292876b
Ermin Mustafić <106275945+ermimust@users.noreply.github.com>
2024-02-17 22:13:24 +0100

    Add files via upload

---
+++ Slicnost medju vrhovima grafova (Matlab)/kodovi/mat_slic.m
@@ -0,0 +1,15 @@
+function S = mat_slic(A, B)
+    % Algoritam opisan u clanku/seminaru
+    
+    nA = max(size(A));
+    nB = max(size(B));
+    
+    Z0 = ones(nB, nA);
+    S = Z0;
+    
+    for k = 1:50
+        M = B*Z0*A' + B'*Z0*A;
+        S = M / norm(M, 'fro');
+        Z0 = S;
+    end
+end
